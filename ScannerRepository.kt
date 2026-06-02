b7c624753b67269a84c1befe81526dfb092c32b3
🪶Arturo  ochoa 🦅 Orange 🍊 <209074126+arturoochoa0606@users.noreply.github.com>
2026-06-02 00:44:45 -0600

    Create ScannerRepository with analyze function

---
+++ ScannerRepository.kt
@@ -0,0 +1,10 @@
+class ScannerRepository {
+
+    suspend fun analyze(url: String): List<Vulnerability> {
+        // Solicitud HTTP
+        // Obtención de headers
+        // Llamada al HeaderAnalyzer
+        return emptyList()
+    }
+
+}
